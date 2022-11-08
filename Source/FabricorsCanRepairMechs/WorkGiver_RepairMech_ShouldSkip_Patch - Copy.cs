529624a324c76841e3c2f27877629709b5f0a8c7
Witek094 <mateusz@graj.cc>
2022-11-08 22:32:03 +0100

    first commit

---
+++ Source/FabricorsCanRepairMechs/WorkGiver_RepairMech_ShouldSkip_Patch - Copy.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace FabricorsCanRepairMechs
+{
+    [HarmonyPatch(typeof(WorkGiver_RepairMech))]
+    [HarmonyPatch(nameof(WorkGiver_RepairMech.ShouldSkip))]
+    public static class WorkGiver_RepairMech_ShouldSkip_Patch
+    {
+        public static bool Prefix(ref bool __result,  Pawn pawn, bool forced = false)
+        {
+            bool result = true;
+            bool ismech = pawn.RaceProps.IsMechanoid;
+            Log.Message("isMech: " + ismech.ToString());
+            if (ismech)
+            {
+                __result = false;
+                result = false;
+            }
+            return result;
+        }
+    }
+}
---
+++ Source/FabricorsCanRepairMechs/WorkGiver_RepairMech_ShouldSkip_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace FabricorsCanRepairMechs
+{
+    [HarmonyPatch(typeof(WorkGiver_RepairMech), "ShouldSkip")]
+    public static class WorkGiver_RepairMech_ShouldSkip_Patch
+    {
+        public static bool Prefix(ref bool __result, Pawn pawn, bool forced = false)
+        {
+            bool result = true;
+            bool ismech = pawn.RaceProps.IsMechanoid;
+            if (ismech)
+            {
+                __result = false;
+                result = false;
+            }
+            return result;
+        }
+    }
+}
